cebb67eb33ea77b58aec68295aacc5b695cb6752
MD Waliullah <waliullah.godev@gmail.com>
2026-02-20 15:29:02 +0600

    feat: custom decorator to extract user from request

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,22 @@
-import { Body, Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { AuthResponseDto } from './dto/auth-response.dto';
+import { Body, Controller, UseGuards } from '@nestjs/common';
+import { RefreshTokenGuard } from './guards/refresh-token-guard';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
-    async RegisterDto(@Body() registerDto: RegisterDto): Promise<AuthResponseDto> {
+    async register(@Body() registerDto: RegisterDto): Promise<AuthResponseDto> {
         return this.authService.register(registerDto);
     }
+
+
+    @UseGuards(RefreshTokenGuard)
+    async refresh() {
+
+    }
+
 
 }
