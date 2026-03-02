b7449e2d1ec2c764c340c68352af2bd07d809215
MD Waliullah <waliullah.godev@gmail.com>
2026-03-02 19:25:09 +0600

    feat: roles.decorators

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -6,6 +6,7 @@ import { ExecutionContext, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { CanActivate } from '@nestjs/common';
 import { Role } from "@prisma/client";
+import { ROLES_KEY } from "../decorators/role.decorators";
 
 
 @Injectable()
@@ -13,7 +14,7 @@ export class RoleGuard implements CanActivate {
     constructor(private reflector: Reflector) { }
 
     canActivate(context: ExecutionContext): boolean {
-        const requireRoles = this.reflector.getAllAndOverride<Role[]>(ROLE_KEY, [
+        const requireRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
             context.getHandler(),
             context.getClass(),
         ]);
